593e89665f1282a261ee33bd74daa1f8d87de507
fdev31 <fdev31@gmail.com>
2025-04-20 19:00:39 +0200

    Avoid repetitions

--- backend/booklovin/main.py
+++ backend/booklovin/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
-
-from .api.v1 import posts
-from .api.v1 import auth
+import importlib
 
 app = FastAPI()
 
 # NOTE:
+# Initial API reference:
 # https://www.notion.so/1db4cee15df381e19c04c0641da536b4?v=1db4cee15df381fa8ce8000cdf16a3de
 
-app.include_router(posts.router, prefix="/api/v1/posts")
-app.include_router(auth.router, prefix="/api/v1/auth")
+providers = ("posts", "auth")
+
+for api_provider in providers:
+    module = importlib.import_module(f"booklovin.api.v1.{api_provider}")
+    app.include_router(module.router, prefix=f"/api/v1/{api_provider}")
