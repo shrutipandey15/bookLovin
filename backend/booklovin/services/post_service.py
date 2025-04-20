b0e5455fbb4cea93a474726c30bf5cbe9f22c2f1
fdev31 <fdev31@gmail.com>
2025-04-20 13:41:43 +0200

    start database integration

--- backend/booklovin/services/post_service.py
+++ backend/booklovin/services/post_service.py
@@ -1,15 +1,13 @@
 from typing import List
 from booklovin.models.post import PostCreate
+from booklovin.core.database import get_database
 
-posts_db = []
 
-
-def create_post(post: PostCreate):
+async def create_post(post: PostCreate) -> str:
     new_post = post.model_dump()
-    new_post["id"] = len(posts_db) + 1
-    posts_db.append(new_post)
-    return new_post
+    result = await get_database().insert_one(new_post)
+    return result.inserted_id.toString()
 
 
-def get_all_posts() -> List[dict]:
-    return posts_db
+async def get_all_posts() -> List[dict]:
+    return []
