795ef819eef6673b69dac352915601613b116a60
fdev31 <fdev31@gmail.com>
2025-04-21 19:49:51 +0200

    add missing User properties

--- backend/booklovin/models/users.py
+++ backend/booklovin/models/users.py
@@ -1,5 +1,12 @@
 from typing import Optional
 from pydantic import BaseModel
+from enum import IntEnum, auto
+
+
+class UserRole(IntEnum):
+    standard = auto()
+    publisher = auto()
+    editor = auto()
 
 
 class UserLogin(BaseModel):
@@ -10,3 +17,6 @@ class UserLogin(BaseModel):
 class User(BaseModel):
     name: str
     description: str
+    role: UserRole = UserRole.standard
+    link: Optional[str] = ""
+    location: Optional[str] = "unknown"
