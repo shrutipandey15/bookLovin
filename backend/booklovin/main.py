1ff485fcfd8f5405cf6b7843b9860d8ea81d2700
fdev31 <fdev31@gmail.com>
2025-04-23 20:00:45 +0200

    implement CORS (Shruti)

--- backend/booklovin/main.py
+++ backend/booklovin/main.py
@@ -1,17 +1,29 @@
-"""Main entry point for the app.
-Loads all the routes."""
-
 from fastapi import FastAPI
 import importlib
+import os
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# NOTE:
-# Initial API reference:
-# https://www.notion.so/1db4cee15df381e19c04c0641da536b4?v=1db4cee15df381fa8ce8000cdf16a3de
+# Load DEBUG setting from environment variable (set to 'True' or 'False' in your environment)
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"  # defaults to False if not set
+
+if DEBUG:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:5173"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 providers = ("posts", "auth")
 
 for api_provider in providers:
-    module = importlib.import_module(f"booklovin.api.v1.{api_provider}")
-    app.include_router(module.router, prefix=f"/api/v1/{api_provider}")
+    try:
+        module = importlib.import_module(f"booklovin.api.v1.{api_provider}")
+        app.include_router(module.router, prefix=f"/api/v1/{api_provider}")
+    except ModuleNotFoundError:
+        print(f"Error: Module booklovin.api.v1.{api_provider} not found!")
+    except AttributeError:
+        print(f"Error: {api_provider} module does not contain a 'router' attribute.")
