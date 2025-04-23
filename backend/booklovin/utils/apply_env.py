56ae4b6e6004d27f75bd21e38598e7d321f333a9
fdev31 <fdev31@gmail.com>
2025-04-23 19:59:35 +0200

    dotenv loading

--- backend/booklovin/utils/apply_env.py
+++ backend/booklovin/utils/apply_env.py
@@ -2,22 +2,6 @@
 Purposely executes on import and starts with an "a" to maximize
 the changes of running as early as possible without breaking style."""
 
-import os
+from dotenv import load_dotenv
 
-# search for .env in current folder, then parents
-env_file = None
-cur_path = os.path.dirname(__file__)
-while True:
-    fname = os.path.join(cur_path, ".env")
-    if os.path.exists(fname):
-        env_file = fname
-        break
-    cur_path = os.path.dirname(cur_path)
-    if cur_path == "/":
-        break
-if env_file:
-    for line in open(env_file):
-        if line.startswith("#") or not line.strip():
-            continue
-        key, value = [x.strip() for x in line.strip().split("=", 1)]
-        os.environ[key] = value
+load_dotenv()
