8d3bfb0261438f812b232f34b7db63c371cef370
fdev31 <fdev31@gmail.com>
2025-05-05 23:41:15 +0200

    clear redis db before tests

--- backend/booklovin/services/database/redis/test_setup.py
+++ backend/booklovin/services/database/redis/test_setup.py
@@ -7,5 +7,6 @@ from .core import DB_NAME, REDIS_SERVER, get_user_key
 
 def setup(user_data):
     client = redis.from_url(f"redis://{REDIS_SERVER[0]}:{REDIS_SERVER[1]}/{DB_NAME}")
+    client.flushdb()
     uid = user_data["email"]
     client.set(get_user_key(uid), dumps(user_data))
